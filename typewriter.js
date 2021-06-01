911fd37bf80930e8bdca31f9114f8142c13d2404
dextergb <dextergb@rogers.com>
2021-06-01 18:03:38 +0000

    learning how to use settimeout and process.stdout.write

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,14 @@
-test
+const sentence = "hello there from lighthouse labs";
+
+let typewriter = (sentence) => {
+  let seconds = 500;
+  for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write((char) + '\n');
+
+    }, seconds);
+    seconds += 100;
+  }
+};
+
+typewriter(sentence);
